10a4c37740bd7278925be77a3b837ea91b6b224e
Federico Barberon <66987826+FedericoBarberon@users.noreply.github.com>
2023-06-20 00:25:19 -0300

    Add google verification meta script

--- rest-countries/src/pages/_document.js
+++ rest-countries/src/pages/_document.js
@@ -5,6 +5,7 @@ export default function Document () {
     <Html lang='en'>
       <Head>
         <link rel='shortcut icon' href='favicon.svg' type='image/x-icon' />
+        <meta name="google-site-verification" content="epe__qGbxCzGfJWtM2C77bj8ICV3FYJ9jdtim0Yrz6E" />
       </Head>
       <body>
         <Main />
